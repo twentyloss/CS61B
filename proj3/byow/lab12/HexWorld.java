c605404f7ef25521f96255fa7ceef429adbdc6f4
twentyloss <1252153341@qq.com>
2024-09-05 21:47:55 +0800

    lab12 done

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,112 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 60;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+
+    public static class Position {
+        public int x;
+        public int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position shiftPosition(int dx, int dy) {
+            return new Position(x + dx, y + dy);
+        }
+
+        public Position getTopNeighbor(int n) {
+            return shiftPosition(0, 2 * n);
+        }
+
+        public Position getRightBottomNeighbor(int n) {
+            return shiftPosition(2 * n - 1, - n);
+        }
+
+        public Position getRightTopNeighbor(int n) {
+            return shiftPosition(2 * n - 1, n);
+        }
+    }
+
+
+    private static void initiateWorld(TETile[][] world) {
+        int width = world.length;
+        int height = world[0].length;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static TETile getRandomTile() {
+        int tile = RANDOM.nextInt(5);
+        switch (tile) {
+            case 0: return Tileset.FLOWER;
+            case 1: return Tileset.MOUNTAIN;
+            case 2: return Tileset.GRASS;
+            case 3: return Tileset.SAND;
+            case 4: return Tileset.TREE;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    private static void addHexagon(TETile[][] world, Position p, int n, TETile tile) {
+        addHexagonHelper(world, p, n, tile, 1);
+    }
+
+    private static void addHexagonHelper(TETile[][] world, Position p, int n, TETile tile, int row) {
+        if (row > n) return;
+        for (int i = 0; i < n + 2 * (row - 1); i++) {
+            int curr_x = p.x + n - row + i;
+            world[curr_x][p.y] = tile;
+            int another_y = p.y + (n - row) * 2 + 1;
+            world[curr_x][another_y] = tile;
+        }
+
+        addHexagonHelper(world, p.shiftPosition(0, 1),n , tile, row + 1);
+    }
+
+    private static void addHexColumn(TETile[][] world, Position p, int n, int count) {
+        if (count < 1) return;
+        addHexagon(world, p, n, getRandomTile());
+        Position next_p = p.getTopNeighbor(n);
+        addHexColumn(world, next_p, n, count - 1);
+    }
+
+    private static void drawHexWorld(TETile[][] world, Position p, int n , int size) {
+        Position col_p = p;
+        addHexColumn(world, col_p, n, size);
+        for (int i = 1; i < size; i++) {
+            col_p = col_p.getRightBottomNeighbor(n);
+            addHexColumn(world, col_p, n, size + i );
+        }
+
+        for (int i = size - 2; i >= 0; i--) {
+            col_p = col_p.getRightTopNeighbor(n);
+            addHexColumn(world, col_p, n, size + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        initiateWorld(world);
+
+        Position p = new Position(20, 20);
+        drawHexWorld(world, p, 3, 4);
+
+        ter.renderFrame(world);
+
+
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,23 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: The position is maintained as an object, and can easily get relative position through the functions.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: The tile of hexagons is like the shape of rooms that generated randomly, and tesselation is to connect rooms and hallways correctly.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: The methods that generate single room or hallway.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: The width can distinguish hallway from room, the width of hallway will be only 1 or 2, but there's no limit to a room.
+Both of them could be rectangle and have walls around them.
